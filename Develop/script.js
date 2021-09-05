3bd2b4dd997cb8e7ae96e1b01b51f16cdc1b10f4
rmnk-nz <rua.manukau20@gmail.com>
2021-09-06 09:46:47 +1000

    included character variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+} 
+
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = [
     '@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.',
@@ -22,14 +30,7 @@ function generatePassword() {
     return;
   }
 }
-// Write password to the #password input
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-} 
 
 
 // Add event listener to generate button
